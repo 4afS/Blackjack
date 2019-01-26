ce6c81e0efd4930fb7efec290ac5b4e416b00e06
4afs <g2x649@gmail.com>
2019-01-26 19:35:15 +0000

    recommit

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Blackjack (blackjack)
 
 main :: IO ()
-main = someFunc
+main = blackjack
--- src/Blackjack.hs
+++ src/Blackjack.hs
@@ -1,16 +1,23 @@
 module Blackjack (blackjack) where
 
+import System.Random.Shuffle (shuffleM)
+import Control.Monad.Random.Class (MonadRandom)
+
 data Card = Ace | Two | Tree | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King
     deriving (Show, Enum, Eq)
 
+-- data Suit = Spade | Diamond | Club | Heart
+
 type Value = Int
 
-data Trump = Trump {
-    _card :: Card,
-    _value :: Value
-    } deriving (Eq, Show)
+-- data Trump = Trump 
+--     {_card :: Card
+--     ,_value :: Value
+--     }
 
-newtype Player = Player { _trumps :: [Trump] }
+newtype Player = Player { _hand :: [Card] }
+
+type Dealer = Player
 
 data Action 
     = Hit
@@ -21,31 +28,27 @@ data Result
     | Draw
     | Lose
 
-class Trumps a where
-    score :: [a] -> Value 
-    cards :: [a] -> [Card]
+-- main = blackjack
 
-instance Trumps Trump where
-    score = sum . map _value
-    cards = map _card
+-- | Shuffled 52 cards excluding Joker.
+shuffledCards :: MonadRandom m => m [Card]
+shuffledCards = shuffleM . concat $ replicate 4 [Ace .. King]
 
 blackjack :: IO ()
-blackjack = undefined
+blackjack = do
+    print =<< shuffledCards
+    print =<< length <$> shuffledCards
 
-main = do
-    let me  = Player [Trump Ace 1, Trump Jack 10, Trump Five 5]
-        dealer  = Player [Trump Two 2, Trump King 10, Trump Seven 7]
-    print $ score $ _trumps me
-    print $ cards $ _trumps me
-    print $ cards $ _trumps dealer 
 
-    let cards = [Ace .. King]
-
-    print $ execute [Trump card | card <- cards] [1..]
-
-execute :: [a -> b] ->[a] -> [b]
+execute :: [a -> b] -> [a] -> [b]
 execute [] _ = []
 execute _ [] = []
 execute (f:fs) (x:xs) = f x : execute fs xs
 
+toValue card = 
+    if card `elem` [Jack, Queen, King] 
+       then 10 
+       else 1 + fromEnum card
 
+judge :: Player -> Dealer -> Result
+judge = undefined
