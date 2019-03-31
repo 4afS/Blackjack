0a07c4cc9dcdc4e83529391b37c27d51ab6a2e5d
4afS <g2x649@gmail.com>
2019-03-31 21:53:29 +0900

    archived a goal

--- src/Blackjack.hs
+++ src/Blackjack.hs
@@ -1,33 +1,116 @@
-module Blackjack (blackjack) where
+module Blackjack
+  ( blackjack
+  ) where
 
-import System.Random.Shuffle (shuffleM)
-import Control.Monad.Random.Class (MonadRandom)
-import Card
+import           Card
+import           Control.Applicative
+import           Control.Monad.Random.Class (MonadRandom)
+import           Control.Monad.State
+import           System.Random.Shuffle      (shuffleM)
 
-newtype Player = Player { _hand :: [Card] }
+data Player
+  = Player { hand :: !Cards }
+  | Dealer { hand :: !Cards }
+  deriving (Show)
 
 type Dealer = Player
 
-data Action 
-    = Hit
-    | Stay
+type Cards = [Card]
 
-data Result 
-    = Win
-    | Draw
-    | Lose
+type Score = Int
+
+data YesOrNo
+  = Yes
+  | No
+  deriving (Eq)
+
+data Result
+  = Win
+  | Draw
+  | Lose
+  deriving (Show)
 
 -- | Shuffled 52 cards excluding Joker.
-shuffledCards :: MonadRandom m => m [Card]
-shuffledCards = shuffleM . concat $ replicate 4 [Ace .. King]
+genShuffledDeck :: MonadRandom m => m Cards
+genShuffledDeck = shuffleM . concat $ replicate 4 [Ace .. King]
 
 blackjack :: IO ()
 blackjack = do
-    print =<< shuffledCards
-    print =<< length <$> shuffledCards
+  putStrLn ""
+  shuffledDeck <- genShuffledDeck
+  (hands, _) <- runStateT playBlackjack shuffledDeck
+  printHands hands
+  printResult $ judge hands
+  where
+    printHands :: (Player, Player) -> IO ()
+    printHands (Player pHand, Dealer dHand) = do
+      putStrLn $ "Player's hand are " ++ show pHand
+      putStrLn $ "Dealer's hand are " ++ show dHand
 
-instance Value Card where
-    toValue card = if card `elem` [Jack, Queen, King] then 10 else 1 + fromEnum card
+    printResult :: Result -> IO ()
+    printResult result = putStrLn $ "Result : " ++ show result
 
-judge :: Player -> Dealer -> Result
-judge player dealer = undefined
+playBlackjack :: StateT Cards IO (Player, Player)
+playBlackjack = do
+  dealer'sHand <- drawCardsByDealer
+  player'sHand <- drawCardsByPlayer
+  return (Player player'sHand, Dealer dealer'sHand)
+
+toScore :: Card -> Score
+toScore card =
+  if card `elem` [Jack, Queen, King]
+    then 10
+    else 1 + fromEnum card
+
+getScore :: Cards -> Score
+getScore = sum . map toScore
+
+drawCards :: Int -> StateT Cards IO Cards
+drawCards n = do
+  cards <- get
+  put $ drop n cards
+  return $ take n cards
+
+drawCardsByDealer :: StateT Cards IO Cards
+drawCardsByDealer = do
+  cards <- get
+  (numberOfCards, tookCards) <- takeCardsWhileSumValueIsNotLessThan17
+  put $ drop numberOfCards cards
+  lift $ putStrLn $ "dealer hands are " ++ show (head tookCards) ++ " and more"
+  return tookCards
+  where
+    takeCardsWhileSumValueIsNotLessThan17 :: StateT Cards IO (Int, Cards)
+    takeCardsWhileSumValueIsNotLessThan17 = do
+      cards <- get
+      let index = length . takeWhile (<= 17) . scanl (+) 0 $ map toScore cards
+      put $ drop index cards
+      return (index, take index cards)
+
+drawCardsByPlayer :: StateT Cards IO Cards
+drawCardsByPlayer = do
+  cards <- get
+  lift $ putStrLn $ "you got " ++ show (take 2 cards)
+  lift $ putStrLn "do you draw more? (y/n) : "
+  yn <- lift $ yesOrNo <$> getLine
+  if yn == Yes
+    then drawCards 3
+    else drawCards 2
+  where
+    yesOrNo :: String -> YesOrNo
+    yesOrNo s
+      | s `elem` ["Yes", "YES", "yes", "Y", "y"] = Yes
+      | s `elem` ["No", "no", "n", "N"] = No
+
+
+
+judge :: (Player, Dealer) -> Result
+judge (player, dealer)
+  | (playerHandsScore > 21) && (dealerHandsScore > 21) = Draw
+  | playerHandsScore > 21 = Lose
+  | dealerHandsScore > 21 = Win
+  | playerHandsScore > dealerHandsScore = Win
+  | playerHandsScore < dealerHandsScore = Lose
+  | otherwise = Draw
+  where
+    playerHandsScore = getScore (hand player)
+    dealerHandsScore = getScore (hand dealer)
--- src/Card.hs
+++ src/Card.hs
@@ -1,10 +1,7 @@
 module Card where
 
-data Card = Ace | Two | Tree | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King
+data Card = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King
     deriving (Show, Enum, Eq)
 
 data Suit = Spade | Diamond | Club | Heart
     deriving (Show, Enum)
-
-class Value a where
-    toValue :: a -> Int
